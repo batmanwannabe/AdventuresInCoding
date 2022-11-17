10647d6437a4384e5baad5910b9aa3e3c8bebc5b
Brian Seabolt <brianmseabolt@gmail.com>
2022-11-17 09:25:14 -0500

    update to .NET 7

---
+++ AIC.Data/Lists/ListExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIC.Data.Lists
+{
+    public class ObjectA
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ObjectB
+    {
+        public int NewId { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    public class ObjectC
+    {
+        public int Counter { get; set; }
+        public string PartNumber { get; set; }
+
+        public List<ObjectA> TonsOfA { get; set; }
+    }
+
+    public class ObjectD
+    {
+        public List<ObjectB> TonsOfB { get; set; }
+        public List<ObjectC> TonsOfC { get; set; }
+    }
+}
